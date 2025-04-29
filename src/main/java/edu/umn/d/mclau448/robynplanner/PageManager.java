f783502e04fbf20b1f8f7d5a4a8276b7f1abbab7
robynae <robynmclaughlin14@gmail.com>
2025-04-29 18:32:02 -0500

    Create addPage method for PageManager class

--- src/main/java/edu/umn/d/mclau448/robynplanner/PageManager.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/PageManager.java
@@ -1,14 +1,20 @@
 package edu.umn.d.mclau448.robynplanner;
 
+import java.util.HashMap;
+
+
 public class PageManager implements Page{
+
+    private HashMap<String, Page> pages = new HashMap<>();
+
     @Override
     public String getPageType() {
-        return "";
+        return "PageManager";
     }
 
     @Override
     public void save() {
-
+        System.out.println("I've no clue what I'm gonna do here.");
     }
 
     @Override
@@ -18,6 +24,11 @@ public class PageManager implements Page{
 
     @Override
     public void onPageClose() {
+        save();
+    }
+
+    public void addPage(String pageType){
 
     }
+
 }
