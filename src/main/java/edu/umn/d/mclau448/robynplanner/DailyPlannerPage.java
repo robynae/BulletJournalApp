5b3c7312e60aff40d0f1506882a3324e9a60ebe0
robynae <robynmclaughlin14@gmail.com>
2025-04-30 11:24:58 -0500

    Create getDate method for DailyPlannerPage

--- src/main/java/edu/umn/d/mclau448/robynplanner/DailyPlannerPage.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/DailyPlannerPage.java
@@ -1,10 +1,18 @@
 package edu.umn.d.mclau448.robynplanner;
 
+import java.util.Date;
+
 public class DailyPlannerPage extends Planner implements Page{
 
 
     public DailyPlannerPage(String plannerTitle) {
         super(plannerTitle);
+    }
+
+    public String getDate(){
+      Date d = new Date();
+      String[] formatDate = d.toString().split(" ");
+      return formatDate[1] + " " + formatDate[2];
     }
 
     @Override
