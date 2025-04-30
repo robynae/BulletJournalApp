4d848b425cc99ba9db5088dc0ade23d984844b1f
robynae <robynmclaughlin14@gmail.com>
2025-04-29 19:17:37 -0500

    Create a Builder for Todo class

--- src/main/java/edu/umn/d/mclau448/robynplanner/Todo.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/Todo.java
@@ -1,4 +1,41 @@
 package edu.umn.d.mclau448.robynplanner;
 
 public class Todo {
+    private String task;
+    private String description;
+    private String deadLine;
+    private Integer priority;
+
+    //Builder for a Todo
+    public static class TodoBuilder {
+        private String task;
+        private String description = null;
+        private String deadLine = null;
+        private Integer priority = null;
+
+        public TodoBuilder(String task){
+            this.task = task;
+        }
+
+        public TodoBuilder setDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public TodoBuilder setDeadline(String deadLine){
+            this.deadLine = deadLine;
+            return this;
+        }
+
+        public TodoBuilder setPriority(Integer priority){
+            this.priority = priority;
+            return this;
+        }
+
+        public Todo build(){
+            return new Todo(this);
+        }
+
+    }
 }
+
