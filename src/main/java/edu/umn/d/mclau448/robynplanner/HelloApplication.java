be65ed6edf2f346313dfe16bd2b25dfeabbcbabb
robynae <robynmclaughlin14@gmail.com>
2025-05-01 13:40:47 -0500

    Okay, hardcode in a page for proof-of-concept. This will be deleted later so that pages can be added dynamically.

--- src/main/java/edu/umn/d/mclau448/robynplanner/HelloApplication.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/HelloApplication.java
@@ -8,10 +8,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -27,12 +26,16 @@ public class HelloApplication extends Application {
 
 
         BorderPane content = new BorderPane();
+        content.setStyle("-fx-background-color: #fdf6f0");
 
         stage.setTitle("BulletJournal App");
 
         //Styling the welcome text
         Label hello = new Label("Welcome.");
-        hello.setStyle("-fx-font-family:'Cambria'; -fx-font-size: 36px; -fx-font-weight: bold; -fx-text-fill: linear-gradient(to bottom right, #F765F2, #EDB67B)");
+        hello.setStyle("-fx-font-family:'Cambria';" +
+                " -fx-font-size: 36px;" +
+                " -fx-font-weight: bold;" +
+                " -fx-text-fill: linear-gradient(to bottom right, #F765F2, #EDB67B)");
 
         //Styling the button
         Button newPage = new Button("+");
@@ -44,21 +47,21 @@ public class HelloApplication extends Application {
                 " -fx-border-width: 5px;" +
                 " -fx-border-color: linear-gradient(to bottom right, #EDB67B, #F765F2)");
 
-        RotateTransition rotateNewPage = new RotateTransition(Duration.millis(500), newPage);
+        RotateTransition rotateNewPage = new RotateTransition(Duration.millis(750), newPage);
         rotateNewPage.setByAngle(360);
         rotateNewPage.setCycleCount(0);
 
         //Create an animation
         newPage.setOnMouseEntered(e -> {
+            newPage.setStyle("-fx-background-color: transparent;" +
+                    "-fx-text-fill: #F765F2;" +
+                    " -fx-border-radius: 50;" +
+                    " -fx-border-width: 5px;" +
+                    " -fx-border-color: linear-gradient(to bottom right, #EDB67B, #F765F2);" +
+                    "-fx-cursor: hand"
+            );
             if(rotateNewPage.getStatus() != Animation.Status.RUNNING){
                 rotateNewPage.playFromStart();
-                newPage.setStyle(
-                        "-fx-background-color: transparent;" +
-                                "-fx-text-fill: #F765F2;" +
-                                "-fx-border-radius: 50;" +
-                                "-fx-border-width: 5px;" +
-                                "-fx-border-color: linear-gradient(to top left, #EDB67B, #F765F2);"
-                );
             }
         });
 
@@ -66,16 +69,54 @@ public class HelloApplication extends Application {
         //Creating the header
         VBox header = new VBox(10);
         header.getChildren().addAll(hello, newPage);
-
         BorderPane.setMargin(header, new Insets(25,0,0,25));
         content.setTop(header);
 
 
         //Creating container for pages
         GridPane pageContainer = new GridPane();
+        pageContainer.setStyle("-fx-background-color: transparent");
         pageContainer.setHgap(10);
         pageContainer.setVgap(10);
+        pageContainer.setPadding(new Insets(50));
 
+        //A Page...to be deleted later as they need to be dynamically added to the interface. But, need this for a proof of concept...
+        Pane page = new Pane();
+        page.setPrefSize(200, 100);
+        page.setStyle("-fx-background-color: linear-gradient(to top left, #EDB67B, #F765F2); -fx-border-radius: 15; -fx-background-radius: 15");
+
+        //I've gone insane in the membrane... create a DropShadow for the Page
+        DropShadow pageShadow = new DropShadow();
+        pageShadow.setRadius(10);
+        pageShadow.setOffsetX(4);
+        pageShadow.setOffsetY(4);
+        pageShadow.setColor(Color.rgb(0,0,0,0.3));
+        page.setEffect(pageShadow);
+
+        Label projectTitle = new Label("Final CS Project");
+        projectTitle.setStyle(
+                "-fx-font-family:'Cambria';" +
+                " -fx-font-size: 24px;" +
+                " -fx-font-weight: bold;" +
+                " -fx-text-fill: #fdf6f0");
+        projectTitle.setLayoutX(20);
+        projectTitle.setLayoutY(40);
+        page.getChildren().add(projectTitle);
+
+        page.setOnMouseEntered(e -> {
+            page.setStyle("-fx-background-color: linear-gradient(to top left, #EDB67B, #F765F2); -fx-border-radius: 15; -fx-background-radius: 15; -fx-cursor: hand");
+            DropShadow hoverShadow = new DropShadow();
+            hoverShadow.setRadius(10);
+            hoverShadow.setOffsetX(7);
+            hoverShadow.setOffsetY(7);
+            hoverShadow.setColor(Color.rgb(0,0,0,0.4));
+            page.setEffect(hoverShadow);
+        });
+
+        page.setOnMouseExited(e -> page.setEffect(pageShadow));
+
+        pageContainer.add(page, 0,0);
+        content.setCenter(pageContainer);
 
         Scene scene = new Scene(content,1280,720);
 
