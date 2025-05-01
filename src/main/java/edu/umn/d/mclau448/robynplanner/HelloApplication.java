0fe1c370b0fbb52b042bc75fa1383a58745110ee
robynae <robynmclaughlin14@gmail.com>
2025-05-01 14:23:30 -0500

    Create PageManagerTests class

--- src/main/java/edu/umn/d/mclau448/robynplanner/HelloApplication.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.animation.Animation;
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
