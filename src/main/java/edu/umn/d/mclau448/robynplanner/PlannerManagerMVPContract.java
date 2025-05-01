65a0b52ce23a651fababf7e9be3cab9d33b0ccb5
robynae <robynmclaughlin14@gmail.com>
2025-05-01 09:23:50 -0500

    Add deletePage method to Presenter, create View interface and add addPage and removePage methods

--- src/main/java/edu/umn/d/mclau448/robynplanner/PlannerManagerMVPContract.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/PlannerManagerMVPContract.java
@@ -1,7 +1,14 @@
 package edu.umn.d.mclau448.robynplanner;
 
 public class PlannerManagerMVPContract {
+
     interface Presenter{
         void createPage(String pageType, String pageTitle);
+        void deletePage(String pageTitle);
+    }
+
+    interface View{
+        void addPage(Page page);
+        void removePage(Page page);
     }
 }
