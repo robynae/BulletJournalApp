ad90793fd6370f5464872f2c7125d95baf210c16
robynae <robynmclaughlin14@gmail.com>
2025-05-02 17:17:30 -0500

    Add JavaDoc comments for ProjectPlanner class

--- src/main/java/edu/umn/d/mclau448/robynplanner/ProjectPlanner.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/ProjectPlanner.java
@@ -1,5 +1,9 @@
 package edu.umn.d.mclau448.robynplanner;
 
+/**
+ * The ProjectPlanner class is a type of Page that acts as a Project Manager. It will just contain a todo list concerning
+ * one specific project. This will have a few unique attributes such as one for when the overall project is due.
+ */
 public class ProjectPlanner extends Planner implements Page{
     public ProjectPlanner(String plannerTitle) {
         super(plannerTitle);
