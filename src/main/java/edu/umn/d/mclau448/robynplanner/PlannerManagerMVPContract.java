0fa0983da2e39b5c1fd9a8216186da4b2cb53f3e
robynae <robynmclaughlin14@gmail.com>
2025-05-02 17:16:10 -0500

    Add JavaDoc comments for PlannerManagerMVPContract (oof that needs renamed) class

--- src/main/java/edu/umn/d/mclau448/robynplanner/PlannerManagerMVPContract.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/PlannerManagerMVPContract.java
@@ -1,12 +1,22 @@
 package edu.umn.d.mclau448.robynplanner;
 
+/**
+ * This is our MVP contract for our PageManager page. So, this controls the view and backend logic concerning our homepage of our app.
+ */
 public class PlannerManagerMVPContract {
 
+    /**
+     * This interface represents the presenter. It allows us to connect the creation and removal of pages between our
+     * back and front ends.
+     */
     interface Presenter{
         void createPage(String pageType, String pageTitle);
         void deletePage(String pageTitle);
     }
 
+    /**
+     * This interface represents the view. These are the methods that will cause update to the GUI when our program is run.
+     */
     interface View{
         void addPage(Page page);
         void removePage(Page page);
