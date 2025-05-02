96fb560e66c6a878f5fd746eaf4fa8a2df4e4970
robynae <robynmclaughlin14@gmail.com>
2025-05-02 17:22:16 -0500

    Add JavaDoc comments for Todo class

--- src/main/java/edu/umn/d/mclau448/robynplanner/Todo.java
+++ src/main/java/edu/umn/d/mclau448/robynplanner/Todo.java
@@ -1,23 +1,46 @@
 package edu.umn.d.mclau448.robynplanner;
 
+/**
+ * This class represents the task items in our Planner Pages in our program.
+ */
 public class Todo {
     private String task;
     private String description;
     private String deadLine;
     private Integer priority;
 
+    /**
+     * Our actual task that needs to be done.
+     *
+     * @return String representing a task
+     */
     public String getTask(){
       return this.task;
     }
 
+    /**
+     * Description of the task to complete
+     *
+     * @return String representing a description
+     */
     public String getDescription(){
         return this.description;
     }
 
+    /**
+     * The deadline for our Todo, it should send the user an alert when the day comes...
+     *
+     * @return String representing the deadline for our task.
+     */
     public String getDeadLine(){
         return this.deadLine;
     }
 
+    /**
+     * Level of priority our task will have. Will be given on a scale from 1 (most important) to 4 (least important)
+     *
+     * @return Integer representing how important our task is.
+     */
     public Integer getPriority(){
         return this.priority;
     }
@@ -29,12 +52,17 @@ public class Todo {
         this.priority = tdBuilder.priority;
     }
 
+    /**
+     * Class to build our Todo object. There are a lot of optional paramters, so I thought using a builder to create
+     * a Todo object would be appropriate.
+     */
     //Builder for a Todo
     public static class TodoBuilder {
         private String task;
         private String description = null;
         private String deadLine = null;
         private Integer priority = null;
+
 
 
         public TodoBuilder(String task){
